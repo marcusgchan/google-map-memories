e76bf18b1040263a91338719e0fc8e9f71aff8b1
Marcus Chan <marcuscchan@gmail.com>
2024-01-20 18:48:44 -0800

    Make public

--- src/server/api/routers/memory.ts
+++ src/server/api/routers/memory.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 import {
   createTRPCRouter,
   protectedProcedure,
+  publicProcedure,
 } from "~/server/api/trpc";
 import { memories } from "~/server/db/schema";
 import { createSchema } from "~/types";
@@ -24,11 +25,12 @@ export const memoryRouter = createTRPCRouter({
       return await ctx.db.select({ id: memories.id, title: memories.title, description: memories.description, date: memories.createdAt })
         .from(memories).where(eq(memories.createdById, ctx.session.user.id)).orderBy(desc(memories.createdAt));
     }),
-  getById: protectedProcedure
+  getById: publicProcedure
     .input(z.object({ id: z.number() }))
     .query(async ({ ctx, input }) => {
-      return await ctx.db.selectDistinct({ id: memories.id, title: memories.title, description: memories.description, date: memories.createdAt })
+      const res = await ctx.db.selectDistinct({ id: memories.id, title: memories.title, description: memories.description, date: memories.createdAt })
         .from(memories).where(eq(memories.id, input.id));
+      return res[0];
     }),
 });
 
