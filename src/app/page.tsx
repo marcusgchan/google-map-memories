a1c16c50e14b95c18b1c7a3c5b7b625b10b598c1
bra40 <brian_rahadi@sfu.ca>
2024-01-20 22:28:08 -0800

    Render profile or login conditionally based on session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,25 @@
 import Link from "next/link";
 import { Anchor } from "~/components/ui/anchor";
 import { InteractiveGlobe } from "~/components/interactive-globe";
+import { getServerAuthSession } from "~/server/auth";
 
 export default async function Home() {
+  const session = await getServerAuthSession();
+
   return (
     <>
-      <header className="mb-4 flex justify-between">
+      <header className="my-4 mx-8 flex justify-between">
         <span>Google Maps Memories</span>
         <span className="flex items-center gap-4">
           <Anchor href="/memories/create">Submit A Memory</Anchor>
-          <Link href="/profile">Profile</Link>
+          {session ? (
+            <Link href="/profile">Profile</Link>
+          ) : (
+            <Link href="/api/auth/signin">Login</Link>
+          )}
         </span>
       </header>
-      <main className="">
+      <main className="mt-4">
         <h1 className="font-['Instrument Serif'] text-center text-4xl">
           Explore the captured memories of Google Maps
         </h1>
